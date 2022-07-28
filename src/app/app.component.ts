5e0e8bfe9aef69b6a673f3c3e37775323b904a6c
Jesica Hoffman <jesica_889@hotmail.com>
2022-07-27 23:01:44 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,71 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Exercise-Wabi';
+  products = [
+    {
+      name: 'Bebidas',
+      id: 1,
+      parentId: null,
+    },
+    {
+      name: 'Comidas',
+      id: 2,
+      parentId: null,
+    },
+    {
+      name: 'Limpieza',
+      id: 3,
+      parentId: null,
+    },
+    {
+      name: 'Gaseosas',
+      id: 100,
+      parentId: 1,
+    },
+    {
+      name: 'Con Alcohol',
+      id: 1010,
+      parentId: 100,
+    },
+    {
+      name: 'Sin Alcohol',
+      id: 1009,
+      parentId: 100,
+    },
+    {
+      name: 'Con Azúcar',
+      id: 101,
+      parentId: 1009,
+    },
+    {
+      name: 'Sin Azucar',
+      id: 103,
+      parentId: 1009,
+    },
+    {
+      name: 'Jugos',
+
+      id: 189,
+      parentId: 103,
+    },
+    {
+      name: 'Energizantes',
+      id: 1222,
+      parentId: 103,
+    },
+    {
+      name: 'Fruta',
+      id: 1223,
+      parentId: 1222,
+    },
+    {
+      name: 'Sin grasa',
+      id: 12231231,
+      parentId: 1223,
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatMenuModule,} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
